744de98f9f9f072beb461a8971ca723dde3f346a
Bishal756 <bk6507074@gmail.com>
2025-10-14 18:02:35 -0400

    OneHundredBottlesOfBeer

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 public class OneHundredBottlesOfBeer {
-  public static void main(String[] args) {
-  }
+  public static void main(String[] args) 
+  { 
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the number of verse you can to print(1-100):");
+    int input= scanner.nextInt(); 
+    System.out.println();
+
+    while(input<1 || input>100){
+      System.out.println("Invalid number");
+      System.out.println("Enter the verse within 1-100:");
+      input=scanner.nextInt();
+    }
+
+    for (int i= 100; i>100-input; i--){ 
+
+      System.out.println( (i)+ " bottles of beer on the wall"); 
+      System.out.println((i)+" bottles of beer"); 
+      System.out.println("If one of those bottles should happen to fall"); 
+      System.out.println((i-1) + " bottles of beer on the wall"); 
+      System.out.println(); 
+}
+   scanner.close(); 
+  } 
 }
