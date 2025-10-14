3f24f7ac83e679e0237065ddadf8d8bb8a823914
Bishal756 <bk6507074@gmail.com>
2025-10-14 18:01:09 -0400

    CountFlips.java

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,24 @@
 public class CountFlips {
   public static void main(String[] args) {
+
+    int headcounts = 0;
+    int tailcounts = 0;
+    final int total_flips = 100;
+
+      Coin coin = new Coin();
+
+    for (int i =1; i<=total_flips; i++){
+      coin.flip();
+
+      if (coin.isHeads()){
+        headcounts++;
+      }
+      else{
+        tailcounts++;
+      }
+    }
+    System.out.println("Here is the result:");
+    System.out.println("Head: "+ headcounts);
+    System.out.println("Tails " + tailcounts);
   }
 }
